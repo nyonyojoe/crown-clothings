f41e17be91fc368d79b59d7be69d3188268067e5
Joel Agbakpe <nyonyojoe@gmail.com>
2022-05-24 22:18:02 +0000

    navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Homepage from "./Pages/Homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
 import ShopPage from "./Pages/shop/shop.component";
-import "./App.css"
+import Header from "./Components/header/header.component";
+import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/shoppage" element={<ShopPage />} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </div>
   );
 }
 
